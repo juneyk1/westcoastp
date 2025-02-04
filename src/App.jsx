52d213090acb2f9056617b47546d2a6a9b30df1e
June Kim <97197559+juneyk1@users.noreply.github.com>
2025-02-04 11:39:09 -0800

    added product info

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import React from 'react';
 import Checkout from './pages/checkout'
 import Landing from './pages/Landing'
+import ProductInfo from './pages/ProductInfo';
 export default function App() {
   return (
     <div>
     <Landing/>
+    <ProductInfo/>
     <Checkout/ >
     </div>
-  )
+  );
 }
 
 
