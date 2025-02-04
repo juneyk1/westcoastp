4ed539be6f818ae39dae75316f37e180dc8bf32b
sophiashaw <eshawsophia@gmail.com>
2025-02-03 19:32:14 -0800

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import React from 'react';
 import Checkout from './pages/checkout'
+import Landing from './pages/Landing'
 export default function App() {
   return (
     <div>
+    <Landing/>
     <Checkout/ >
     </div>
   )
