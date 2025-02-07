8bba826ebad6514c7cea4a36426efb62a1370ebb
June Kim <97197559+juneyk1@users.noreply.github.com>
2025-02-07 12:30:24 -0800

    added some connectivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from './pages/checkout'
 import Landing from './pages/Landing'
 import ProductInfo from './pages/ProductInfo';
 export default function App() {
   return (
-    <div>
-    <Landing/>
-    <ProductInfo/>
-    <Checkout/ >
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/product-info" element={<ProductInfo />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Router>
   );
 }
 
