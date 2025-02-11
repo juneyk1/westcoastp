64dea05bdda4e61f6ab1d1b6288b7d50f03ff46f
Miguel Ruiz Licea <Miguelruizlicea@gmail.com>
2025-02-11 12:59:13 -0800

    Added login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import './App.css'
-import React from 'react';
-import Checkout from './pages/checkout'
-import Landing from './pages/Landing'
-import ProductInfo from './pages/ProductInfo';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./pages/checkout";
+import Landing from "./pages/Landing";
+import ProductInfo from "./pages/ProductInfo";
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/signUp";
 export default function App() {
   return (
-    <div>
-    <Landing/>
-    <ProductInfo/>
-    <Checkout/ >
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />{" "}
+        {/* Landing page at the root URL */}
+        <Route path="/product/:id" element={<ProductInfo />} />{" "}
+        {/* Example: ProductInfo with a dynamic ID */}
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
-
-
