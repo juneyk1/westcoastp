011fa1d8b02a6412ae447088c007bbbcf8683433
June Kim <97197559+juneyk1@users.noreply.github.com>
2025-05-02 07:04:10 -0700

    omitted userauth for now

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,26 @@ import Account from "./pages/Account";
 import Appendices from "./pages/Appendices";
 import CheckoutResponse from "./pages/checkout-response";
 import { AuthContextProvider } from "./contexts/AuthContexts";
+import { CartProvider } from "../src/contexts/CartContext";
 export default function App() {
   return (
     <AuthContextProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/subscribe" element={<Subscription />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Account" element={<Account />} />
-          <Route path="/notices" element={<Appendices />} />
-          <Route path="/checkout-response" element={<CheckoutResponse />} />
-          <Route path="/products/:productName" element={<ProductInfo />} />
-        </Routes>
-      </Router>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/subscribe" element={<Subscription />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/Account" element={<Account />} />
+            <Route path="/notices" element={<Appendices />} />
+            <Route path="/checkout-response" element={<CheckoutResponse />} />
+            <Route path="/products/:productName" element={<ProductInfo />} />
+          </Routes>
+          </Router>
+        </CartProvider>
     </AuthContextProvider>
   );
 }
